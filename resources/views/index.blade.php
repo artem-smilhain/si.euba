1afafef4cb4cd2fe330e68bb6a6dc0e21a7c6650
Artem Smilhain <smilhain@gmail.com>
2024-11-22 02:55:46 +0100

    hello world - app index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SI EUBA PROJECT</title>
+    <style>
+        @media screen and (max-width: 700px) {
+            #inner_block{
+                flex-direction: column;
+                padding-top: 30px;
+            }
+            #inner_block *{
+                margin: 0;
+                padding: 0;
+            }
+        }
+    </style>
+</head>
+<body>
+    <div style="text-align: center; padding-top: 100px;">
+        <h1 style="margin: 0; padding: 0;">Hello World!</h1>
+        <div id="inner_block" style="display: inline-flex; align-items: center; gap: 8px; font-size: 20px;">
+            <p><b><i>Made with</i></b></p>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M16.5 3C13.605 3 12 5.09 12 5.09C12 5.09 10.395 3 7.5 3C4.462 3 2 5.462 2 8.5C2 14 12 21 12 21C12 21 22 14 22 8.5C22 5.462 19.538 3 16.5 3ZM12 18.518C8.517 15.845 4 11.406 4 8.5C4 6.57 5.57 5 7.5 5C9.902 5 12 7.907 12 7.907C12 7.907 14.14 5 16.5 5C18.43 5 20 6.57 20 8.5C20 11.406 15.483 15.845 12 18.518Z" fill="#FB5252"/>
+            </svg>
+            <p><b><i>by Simona Šorfová, Vanessa Šulanová and Artem Smilhain</i></b></p>
+        </div>
+    </div>
+</body>
+</html>
